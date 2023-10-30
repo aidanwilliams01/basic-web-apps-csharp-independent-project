236a44f76002bb098076aa8d495e406e33b25405
Aidan Williams <aidanwilliams01@gmail.com>
2023-10-30 15:17:11 -0700

    pass GetId test

--- VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
+++ VendorAndOrderTracker.Tests/ModelTests/VendorTests.cs
@@ -4,14 +4,28 @@ using VendorAndOrderTracker.Models;
 namespace VendorAndOrderTracker.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
 
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+    
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor();
+      Vendor newVendor = new Vendor("test name", "test description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetId_ReturnsId_Int()
+    {
+      string name = "test";
+      Vendor newVendor = new Vendor(name, "test");
+      int result = newVendor.Id;
+      Assert.AreEqual(1, result);
     }
   }
 }
--- VendorAndOrderTracker/Models/Vendor.cs
+++ VendorAndOrderTracker/Models/Vendor.cs
@@ -2,9 +2,22 @@ namespace VendorAndOrderTracker.Models
 {
   public class Vendor
   {
-    public Vendor()
+    private static List<Vendor> _instances = new List<Vendor>{};
+    public string Name {get; set;}
+    public string Description {get; set;}
+    public int Id {get;}
+
+    public Vendor(string name, string description)
     {
-      
+      Name = name;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
